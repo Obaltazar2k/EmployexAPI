7033cc5fd71ebc16e33f0a67bcdbf12f8b1d992c
OMAR BALTAZAR ISLAS <zS18012180@estudiantes.uv.mx>
2021-06-09 01:16:28 -0500

    get organization user funcion terminada

--- swagger_server/controllers/independient_user_controller.py
+++ swagger_server/controllers/independient_user_controller.py
@@ -1,9 +1,9 @@
-from swagger_server.models.user import User
 import connexion
 import json
 
 from flask import Response
 from swagger_server.models.independient_user import IndependientUser  # noqa: E501
+from swagger_server.models.user import User
 from swagger_server.models.media import Media as MediaModels
 from swagger_server.models.laboral_experience import LaboralExperience
 from swagger_server.models.education import Education
@@ -14,7 +14,6 @@ from http import HTTPStatus
 from flask_jwt_extended import jwt_required
 from swagger_server.data.db import Media, Independiente, Usuario, Experiencialaboral, Educacion, Seccion, Certificacion,database
 from peewee import DoesNotExist
-from datetime import date
 
 @jwt_required()
 def get_independint_user_by_id(user_id):  # noqa: E501
