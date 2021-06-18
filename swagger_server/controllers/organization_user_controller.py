e20eb135f0210da601ccc7fa79b7785b4348cdc7
Ricardo <ricardorzan@gmail.com>
2021-06-18 01:37:20 -0500

    Hotfix

--- swagger_server/controllers/organization_user_controller.py
+++ swagger_server/controllers/organization_user_controller.py
@@ -74,7 +74,7 @@ def register_organization_user(body):  # noqa: E501
     response = Response(status=HTTPStatus.UNAUTHORIZED.value)
     if connexion.request.is_json:
         body = OrganizationUser.from_dict(connexion.request.get_json())  # noqa: E501
-        list_accounts = Organizacion.select().where(OrganizationUser.usuariocorreo == body.user.email)
+        list_accounts = Organizacion.select().where(Organizacion.usuariocorreo == body.user.email)
         if list_accounts.exists():
             return response
         else:
